21d48567faf9023ae4508dfe6308218b949da7e0
Neel Smith <dnsmith.neel@gmail.com>
2014-08-21 02:11:41 -0400

    Make bash scripts directly executable.

--- scripts/build-hmt.sh
+++ scripts/build-hmt.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env /bin/bash
-GRADLE=`which gradle`
 
+GRADLE=`which gradle`
+GIT=`which git`
+
+BRANCH="master"
+if [ "$#" -eq 1 ]; then
+    BRANCH=$1
+fi
+echo "Checking out branch ${BRANCH} of hmt-archive"
 cd /vagrant/repositories/hmt-archive
+$GIT checkout $BRANCH
+
 $GRADLE clean
 $GRADLE allTtl
 
