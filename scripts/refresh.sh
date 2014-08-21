708e31f45cfc620c14189e1cb65481ec5cd6cba1
Neel Smith <dnsmith.neel@gmail.com>
2014-08-21 02:18:10 -0400

    Tidy up refresh.sh.

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -1,63 +1,10 @@
 #!/usr/bin/env /bin/bash
 
-GIT=`which git`
+# Instal web pages
+/vagrant/scripts/web.sh
 
-## Install web pages:
-echo "Copying web pages into place.."
-sudo cp -r -u /vagrant/system/web/* /var/www/html
+# Install jena
+/vagrant/scripts/get-jena.sh
 
-
-#http://apache.arvixe.com/jena/binaries/
-
-## Check out all required repos:
-# hmt-archive
-if [ -d "/vagrant/repositories/hmt-archive" ]; then
-    echo "Checking for updates to hmt-archive"
-    cd /vagrant/repositories/hmt-archive
-    $GIT pull
-else
-    echo "Installing hmt-archive"
-    cd /vagrant/repositories
-    echo  Running  $GIT clone https://github.com/homermultitext/hmt-archive.git
-    $GIT clone https://github.com/homermultitext/hmt-archive.git
-fi
-
-
-
-# cite manager
-if [ -d "/vagrant/repositories/citemgr" ]; then
-    echo "Checking for updates to citemgr"
-    cd /vagrant/repositories/citemgr
-    $GIT pull
-else
-    echo "Installing citemgr"
-    cd /vagrant/repositories
-    echo  Running  $GIT https://github.com/cite-architecture/citemgr.git
-    $GIT clone https://github.com/cite-architecture/citemgr.git
-fi
-
-
-# https://github.com/cite-architecture/citeservlet.git
-if [ -d "/vagrant/repositories/citeservlet" ]; then
-    echo "Checking for updates to citeservlet"
-    cd /vagrant/repositories/citeservlet
-    $GIT pull
-else
-    echo "Installing citeservlet"
-    cd /vagrant/repositories
-    echo  Running  $GIT https://github.com/cite-architecture/citeservlet.git
-    $GIT clone https://github.com/cite-architecture/citeservlet.git
-fi
-
-
-# https://github.com/homermultitext/hmt-digital
-if [ -d "/vagrant/repositories/hmt-digital" ]; then
-    echo "Checking for updates to hmt-digital"
-    cd /vagrant/repositories/hmt-digital
-    $GIT pull
-else
-    echo "Installing hmt-digital"
-    cd /vagrant/repositories
-    echo  Running  $GIT  https://github.com/homermultitext/hmt-digital
-    $GIT clone  https://github.com/homermultitext/hmt-digital
-fi
+# Pull on all repositories
+/vagrant/scripts/pull-repos.sh
