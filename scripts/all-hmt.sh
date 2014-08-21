6887c2b57f6879cf189d7bb3c5dea857add4c22c
Neel Smith <dnsmith.neel@gmail.com>
2014-08-21 03:25:31 -0400

    Tidy names.

--- scripts/all-hmt.sh
+++ scripts/all-hmt.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env /bin/bash
 
 /vagrant/scripts/build-ttl.sh
-/vagrant/scripts/load-hmt.sh
+/vagrant/scripts/load-ttl.sh
 
 /vagrant/scripts/build-war.sh
-/vagrant/scripts/run-hmt.sh
+/vagrant/scripts/run-war.sh
