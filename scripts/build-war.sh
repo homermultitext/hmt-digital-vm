68ab933e4cac26bb4b3b1fca0fb0495cf3db32a7
Neel Smith <dnsmith.neel@gmail.com>
2014-08-24 20:05:07 -0400

    Add optional param to build war from specified branch of hmt-digital.

--- scripts/build-war.sh
+++ scripts/build-war.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env /bin/bash
 #
-# install hmt-digital.war in tomcat and restart tc
+# build hmt-digital servlet.  If an argument is given, it's the branch of hmt-digital
+# to use.
 #
 
+
+
+if [ $# -eq 1 ]; then
+    BRANCH=$1
+else 
+    BRANCH=master
+fi
 HMTDIGITAL=/vagrant/repositories/hmt-digital
 
+GIT=`which git`
+
+
+echo "In ${HMTDIGITAL}, checkout $BRANCH"
+cd $HMTDIGITAL
+$GIT checkout $BRANCH
 
 GRADLE=`which gradle`
-
 cd /vagrant/repositories/citeservlet
 
 echo "Build war file.  Clean out previous build."
